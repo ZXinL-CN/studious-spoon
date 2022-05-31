ef925bf3992954948d2ce067c27b4b3956afec35
ZhuXinLiang <zxl@datacreating.com>
2022-05-31 16:51:22 +0800

    feat(题目):浅拷贝.

---
+++ shallowClone/README.md
@@ -0,0 +1,7 @@
+## 浅拷贝
+> 描述
+> 请补全JavaScript代码，要求实现一个对象参数的浅拷贝并返回拷贝之后的新对象。
+> 注意：
+> 1. 参数可能包含函数、正则、日期、ES6新对象
+
+[题目地址点击查看](https://www.nowcoder.com/practice/434d236e52994a9791a055f1f7adf985?tpId=274&tqId=39535&rp=1&ru=/exam/oj&qru=/exam/oj&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26pageSize%3D50%26search%3D%26tab%3D%25E5%2589%258D%25E7%25AB%25AF%25E7%25AF%2587%26topicId%3D274&difficulty=undefined&judgeStatus=undefined&tags=&title=)
---
+++ shallowClone/index.ts
@@ -0,0 +1,22 @@
+function shallowClone(target: any) {
+  if (typeof target !== 'object' || target === null) return target
+  const constructor = target.constructor
+  if (/^(Function | Date | RegExp | Map | Set)$/i.test(constructor.name)) return target
+  const cloneTarget: any = Array.isArray(target) ? [] : {}
+  for (const prop in target) {
+    cloneTarget[prop] = target[prop]
+  }
+  return cloneTarget
+}
+
+const obj = {
+  a: 1,
+  b: true,
+  c: 'Jay',
+  d: (val: any) => (val),
+  e: null
+}
+console.log(obj);
+console.log(shallowClone(obj));
+console.log(obj === shallowClone(obj)); // false
+console.log(obj.d === shallowClone(obj).d); // true
