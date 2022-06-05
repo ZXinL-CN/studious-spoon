9d3adf7d1601d2f4569e3e76a5d3789fff39770a
ZhuXinLiang <zxl@datacreating.com>
2022-06-06 00:43:38 +0800

    feat(题目):深拷贝.

---
+++ completeDeepClone/README.md
@@ -0,0 +1,9 @@
+## 深拷贝
+
+>描述
+>请补全JavaScript代码，要求实现对象参数的深拷贝并返回拷贝之后的新对象。
+>注意：
+>1. 需要考虑函数、正则、日期、ES6新对象
+>2. 需要考虑循环引用问题
+
+[题目地址点击查看](https://www.nowcoder.com/practice/70ca77b52d424ced8ebb348cd77c1dc5?tpId=274&tqId=39537&rp=1&ru=/exam/oj&qru=/exam/oj&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26tab%3D%25E5%2589%258D%25E7%25AB%25AF%25E7%25AF%2587%26topicId%3D274&difficulty=undefined&judgeStatus=undefined&tags=&title=)
---
+++ completeDeepClone/index.ts
@@ -0,0 +1,40 @@
+export const _completeDeepClone = (target: any, map = new WeakMap()) => {
+  if (typeof target !== 'object' || target === null) return target
+  const constructor = target.constructor
+  if (/^(Function | Date | RegExp | Map | Set)$/i.test(constructor.name)) return new constructor(target)
+  if (map.get(target)) return map.get(target)
+  map.set(target, true)
+  const cloneTarget: any = Array.isArray(target) ? [] : Object.create(null)
+  for (const prop in target) {
+    if (target.hasOwnProperty(prop)) {
+      cloneTarget[prop] = _completeDeepClone(target[prop], map)
+    }
+  }
+  return cloneTarget
+}
+
+const obj = {
+  a: 1,
+  b: true,
+  c: 'Jay',
+  d: {
+    da: 'Tom',
+    db: null,
+    dc: [1, 2, 3]
+  },
+  e: null,
+  f: [1, 2, {
+    fa: [1, 2, 3],
+    fb: [{
+      fc: 99
+    }]
+  }],
+  g: (val: any) => val,
+  h: new Date(),
+  i: new RegExp(/[0-9]/),
+}
+console.log(obj);
+console.log(_completeDeepClone(obj));
+console.log(obj === _completeDeepClone(obj)); // false
+console.log(obj.d === _completeDeepClone(obj).d); // false
+console.log(obj.f === _completeDeepClone(obj).f); // false
